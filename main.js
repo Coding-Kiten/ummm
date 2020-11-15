75bce7e03d17de520194522bfdf1c96c98f2541f
Coding-Kiten <71798143+Coding-Kiten@users.noreply.github.com>
2020-11-14 20:28:35 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function um(){
+    var replace_name=dis.replace("Your order has been submited!");
+    console.log(replace_name);  
+}
+function ugo(){
+    var replace_name=g.replace("Your order has been submited!");
+    console.log(replace_name);
+} 
+function umm(){
+    var replace_name=u.replace("Your order has been submited!");
+    console.log(replace_name);
+} 
